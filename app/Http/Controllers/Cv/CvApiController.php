45751041ecb01d1af2f5b74e9886421b6236cb11
Marco Hernandez <marcohern@gmail.com>
2026-09-16 15:00:52 -0500

    added API endpoints to test the data

---
+++ app/Http/Controllers/Cv/CvApiController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Cv;
+
+use App\Http\Controllers\Controller;
+use App\Models\Cv\CvProfile;
+use App\Models\Cv\CvJobTitle;
+use Illuminate\Http\Request;
+
+class CvApiController extends Controller
+{
+    //
+    public function __construct() {
+
+    }
+
+    public function profiles() {
+      return CvProfile::all();
+    }
+
+    public function jobTitles() {
+      return CvJobTitle::all();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\Cv\CvApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/cv_profiles', [CvApiController::class, 'profiles']);
+Route::get('/cv_job_titles', [CvApiController::class, 'jobTitles']);
+
