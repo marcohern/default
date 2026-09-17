e9c61964d946a07c69db14b9dc9cbf7c21c5ef9f
Marco Hernandez <marcohern@gmail.com>
2026-09-16 23:07:55 -0500

    PortraitImage component

---
+++ resources/views/components/cv/portrait-image.blade.php
@@ -0,0 +1,6 @@
+<!-- Background -->
+<div class="background-bg">
+  <div class="background-filter">
+    <div class="background-img" style="background-image: url({{ Vite::asset('resources/images/cv/marco-820x1080.jpeg') }});"></div>
+  </div>
+</div>
--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -81,12 +81,7 @@
 		<!-- Wrapper -->
 		<div class="wrapper">
 
-			<!-- Background -->
-			<div class="background-bg">
-				<div class="background-filter">
-					<div class="background-img" style="background-image: url({{ Vite::asset('resources/images/cv/marco-820x1080.jpeg') }});"></div>
-				</div>
-			</div>
+      <x-cv.portrait-image/>
 
 			<!-- Section Started -->
 			<div class="section started" id="section-started">
