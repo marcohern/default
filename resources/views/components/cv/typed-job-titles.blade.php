de49062a3ff4e28c3ed8ee647935b15d719f3ec2
Marco Hernandez <marcohern@gmail.com>
2026-09-16 23:40:08 -0500

    added Job Title string key translations

--- resources/views/components/cv/typed-job-titles.blade.php
+++ resources/views/components/cv/typed-job-titles.blade.php
@@ -2,7 +2,7 @@
 <div class="h-subtitles">
   <div class="h-subtitle typing-subtitle">
     @foreach ($jobTitles as $jobTitle)
-      <p>{{ $jobTitle->title }}</p>
+      <p>{{ __($jobTitle->title) }}</p>
     @endforeach
   </div>
   <span class="typed-subtitle"></span>
