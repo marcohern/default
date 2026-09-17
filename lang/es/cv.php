5e0d341f74b7b4c82469f97451f3f30f30034d60
Marco Hernandez <marcohern@gmail.com>
2026-09-17 00:21:47 -0500

    removed cv.php translation that is now in json

--- lang/es/cv.php
+++ lang/es/cv.php
@@ -1,9 +1,6 @@
 <?php
 
 return [
-  'profile_intro' => '¡Hola! Soy un ingeniero de software con más de 20 años de experiencia en la industria.' .
-        ' He trabajado en una variedad de proyectos, desde pequeñas startups hasta grandes empresas.' .
-        ' Me apasiona la tecnología y siempre estoy buscando nuevos desafíos.',
   'under_maintenance' => 'En Mantenimiento',
   'menu-links' => [
     'home' => 'Inicio',
