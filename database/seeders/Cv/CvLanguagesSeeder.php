82fdd995398a6814fab608eccef07397539436f4
Marco Hernandez <marcohern@gmail.com>
2026-09-18 15:48:59 -0500

    language label update

--- database/seeders/Cv/CvLanguagesSeeder.php
+++ database/seeders/Cv/CvLanguagesSeeder.php
@@ -18,7 +18,7 @@ class CvLanguagesSeeder extends Seeder
         ['profile_id' => 1, 'name' => 'English', 'locale' => 'en', 'ord'=> 1, 'score' => 100,
           'description' => 'Learned english from very young, got a C2 on my IELTS test. No cap.',
         ],
-        ['profile_id' => 1, 'name' => 'Spanish', 'locale' => 'es', 'ord'=> 2, 'score' => 100,
+        ['profile_id' => 1, 'name' => 'Español', 'locale' => 'es', 'ord'=> 2, 'score' => 100,
           'description' => 'Mi idioma nativo.',
         ],
         ['profile_id' => 1, 'name' => 'Français', 'locale' => 'fr', 'ord'=> 3, 'score' => 8,
