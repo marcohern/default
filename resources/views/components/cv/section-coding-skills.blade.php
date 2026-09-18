2c2ae6acd2d4a3bdd9869b6c7434174d92fc996d
Marco Hernandez <marcohern@gmail.com>
2026-09-17 21:55:51 -0500

    Condig Skills Section created

---
+++ resources/views/components/cv/section-coding-skills.blade.php
@@ -0,0 +1,91 @@
+
+<!-- Section Coding Skills -->
+<div class="section skills" id="section-skills-code">
+  <div class="content">
+    
+    <!-- title -->
+    <div class="titles">
+      <div class="title">Coding Skills</div>
+      <div class="subtitle">Developing on</div>
+    </div>
+
+    <!-- skills items-->
+    <div class="skills circles">
+      <ul>
+        <li>
+          <div class="progress p90"> <!-- p90 = 90% circle fill color -->
+            <div class="percentage"></div>
+            <span>90%</span>
+          </div>
+          <div class="name">WordPress</div>
+          <div class="single-post-text">
+            <p>
+              Etiam sit amet orci eget eros faucibus tincidunt.
+            </p>
+          </div>
+        </li>
+        <li>
+          <div class="progress p75"> <!-- p75 = 75% circle fill color -->
+            <div class="percentage"></div>
+            <span>75%</span>
+          </div>
+          <div class="name">PHP, MYSQL</div>
+          <div class="single-post-text">
+            <p>
+              Etiam sit amet orci eget eros faucibus tincidunt.
+            </p>
+          </div>
+        </li>
+        <li>
+          <div class="progress p85"> <!-- p85 = 85% circle fill color -->
+            <div class="percentage"></div>
+            <span>85%</span>
+          </div>
+          <div class="name">JavaScript</div>
+          <div class="single-post-text">
+            <p>
+              Etiam sit amet orci eget eros faucibus tincidunt.
+            </p>
+          </div>
+        </li>
+        <li>
+          <div class="progress p80"> <!-- p80 = 80% circle fill color -->
+            <div class="percentage"></div>
+            <span>80%</span>
+          </div>
+          <div class="name">Angular</div>
+          <div class="single-post-text">
+            <p>
+              Etiam sit amet orci eget eros faucibus tincidunt.
+            </p>
+          </div>
+        </li>
+        <li>
+          <div class="progress p95"> <!-- p95 = 95% circle fill color -->
+            <div class="percentage"></div>
+            <span>95%</span>
+          </div>
+          <div class="name">HTML, CSS</div>
+          <div class="single-post-text">
+            <p>
+              Etiam sit amet orci eget eros faucibus tincidunt.
+            </p>
+          </div>
+        </li>
+        <li>
+          <div class="progress p90"> <!-- p90 = 90% circle fill color -->
+            <div class="percentage"></div>
+            <span>90%</span>
+          </div>
+          <div class="name">Python</div>
+          <div class="single-post-text">
+            <p>
+              Etiam sit amet orci eget eros faucibus tincidunt.
+            </p>
+          </div>
+        </li>
+      </ul>
+    </div>
+
+  </div>
+</div>
