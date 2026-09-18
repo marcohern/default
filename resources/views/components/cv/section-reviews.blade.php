a7a7f3c78f3df0f922d2caccaae64d873da3173b
Marco Hernandez <marcohern@gmail.com>
2026-09-17 21:58:18 -0500

    SectionReviews

---
+++ resources/views/components/cv/section-reviews.blade.php
@@ -0,0 +1,86 @@
+<!-- Section Testimonials -->
+<div class="section testimonials" id="section-testimonials">
+  <div class="content">
+
+    <!-- title -->
+    <div class="titles">
+      <div class="title">Reviews</div>
+      <div class="subtitle">What customers say</div>
+    </div>
+
+    <!-- testimonials items -->
+    <div class="content-carousel">
+      <div class="owl-carousel" data-slidesView="2" data-slidesview_mobile="1">
+
+        <div class="item">
+          <div class="reviews-item">
+            <div class="image">
+              <img src="{{ Vite::asset('resources/images/cv/rev1.png') }}" alt="">
+            </div>
+            <div class="info">
+              <div class="name">Joseph Mackey</div>
+              <div class="company">Architect</div>
+            </div>
+            <div class="text">
+              Excellent all around! What stood out most was Jake's excellent service. He made sure our order was placed in time, even though we were ordering on a holiday.
+            </div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="reviews-item">
+            <div class="image">
+              <img src="{{ Vite::asset('resources/images/cv/rev1.png') }}" alt="">
+            </div>
+            <div class="info">
+              <div class="name">James Green</div>
+              <div class="company">Art Director</div>
+            </div>
+            <div class="text">
+              Excellent all around! What stood out most was Jake's excellent service. He made sure our order was placed in time, even though we were ordering on a holiday.
+            </div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="reviews-item">
+            <div class="image">
+              <img src="{{ Vite::asset('resources/images/cv/rev1.png') }}" alt="">
+            </div>
+            <div class="info">
+              <div class="name">Joseph Mackey</div>
+              <div class="company">Architect</div>
+            </div>
+            <div class="text">
+              Excellent all around! What stood out most was Jake's excellent service. He made sure our order was placed in time, even though we were ordering on a holiday.
+            </div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="reviews-item">
+            <div class="image">
+              <img src="{{ Vite::asset('resources/images/cv/rev1.png') }}" alt="">
+            </div>
+            <div class="info">
+              <div class="name">James Green</div>
+              <div class="company">Art Director</div>
+            </div>
+            <div class="text">
+              Excellent all around! What stood out most was Jake's excellent service. He made sure our order was placed in time, even though we were ordering on a holiday.
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <!-- navigation -->
+      <div class="navs">
+        <span class="prev fas fa-chevron-left"></span>
+        <span class="next fas fa-chevron-right"></span>
+      </div>
+
+    </div>
+    
+  </div>
+</div>
