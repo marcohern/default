1bffb4969c3b08f4deeeffda79f61fd33cb88a29
Marco Hernandez <marcohern@gmail.com>
2026-09-17 20:58:02 -0500

    design skills sections secured

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -69,76 +69,7 @@
 
 			<x-cv.section-education :education="$profile->educationExperiences"/>
 
-			<!-- Section Design Skills -->
-			<div class="section skills" id="section-skills">
-				<div class="content">
-
-					<!-- title -->
-					<div class="titles">
-						<div class="title">Design Skills</div>
-						<div class="subtitle">Creative ability</div>
-					</div>
-
-					<!-- skills items -->
-					<div class="skills percent">
-						<ul>
-							<li>
-								<div class="name">Web Layout</div>
-								<div class="single-post-text">
-									<p>
-										Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh.
-									</p>
-								</div>
-								<div class="progress">
-									<div class="percentage" style="width: 90%;">
-										<span class="percent">90%</span>
-									</div>
-								</div>
-							</li>
-							<li>
-								<div class="name">Illustrations</div>
-								<div class="single-post-text">
-									<p>
-										Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh.
-									</p>
-								</div>
-								<div class="progress">
-									<div class="percentage" style="width: 70%;">
-										<span class="percent">70%</span>
-									</div>
-								</div>
-							</li>
-							<li>
-								<div class="name">Photoshop</div>
-								<div class="single-post-text">
-									<p>
-										Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh.
-									</p>
-								</div>
-								<div class="progress">
-									<div class="percentage" style="width: 95%;">
-										<span class="percent">95%</span>
-									</div>
-								</div>
-							</li>
-							<li>
-								<div class="name">Graphic Design</div>
-								<div class="single-post-text">
-									<p>
-										Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh.
-									</p>
-								</div>
-								<div class="progress">
-									<div class="percentage" style="width: 85%;">
-										<span class="percent">85%</span>
-									</div>
-								</div>
-							</li>
-						</ul>
-					</div>
-
-				</div>
-			</div>
+      <!--x-cv.section-design-skills/-->
 
 			<!-- Section Languages Skills -->
 			<div class="section skills" id="section-skills-lang">
