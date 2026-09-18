07dd5a5d44e83e38064832ea99e89ad38862fa5f
Marco Hernandez <marcohern@gmail.com>
2026-09-17 22:01:19 -0500

    SectionPortfolio and SectionClients

---
+++ resources/views/components/cv/section-clients.blade.php
@@ -0,0 +1,92 @@
+
+<!-- Section Clients -->
+<div class="section clients" id="section-clients">
+  <div class="content">
+
+    <!-- title -->
+    <div class="titles">
+      <div class="title">Clients</div>
+      <div class="subtitle">My partners</div>
+    </div>
+
+    <!-- clients items -->
+    <div class="content-carousel">
+      <div class="owl-carousel" data-slidesview="4" data-slidesview_mobile="2">
+
+        <div class="item">
+          <div class="clients-item">
+            <a target="_blank" href="#">
+              <img src="{{ Vite::asset('resources/images/cv/client2.png') }}" alt="" />
+            </a>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="clients-item">
+            <a target="_blank" href="#">
+              <img src="{{ Vite::asset('resources/images/cv/client3.png') }}" alt="" />
+            </a>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="clients-item">
+            <a target="_blank" href="#">
+              <img src="{{ Vite::asset('resources/images/cv/client1.png') }}" alt="" />
+            </a>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="clients-item">
+            <a target="_blank" href="#">
+              <img src="{{ Vite::asset('resources/images/cv/client4.png') }}" alt="" />
+            </a>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="clients-item">
+            <a target="_blank" href="#">
+              <img src="{{ Vite::asset('resources/images/cv/client2.png') }}" alt="" />
+            </a>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="clients-item">
+            <a target="_blank" href="#">
+              <img src="{{ Vite::asset('resources/images/cv/client3.png') }}" alt="" />
+            </a>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="clients-item">
+            <a target="_blank" href="#">
+              <img src="{{ Vite::asset('resources/images/cv/client1.png') }}" alt="" />
+            </a>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="clients-item">
+            <a target="_blank" href="#">
+              <img src="{{ Vite::asset('resources/images/cv/client4.png') }}" alt="" />
+            </a>
+          </div>
+        </div>
+
+      </div>
+
+      <!-- navigation -->
+      <div class="navs">
+        <span class="prev fas fa-chevron-left"></span>
+        <span class="next fas fa-chevron-right"></span>
+      </div>
+
+    </div>
+    
+    <div class="clear"></div>
+  </div>
+</div>
